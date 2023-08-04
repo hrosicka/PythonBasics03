f8070b2bef06b2ffb2ba5b40fda8424a51ab6bcd
Hanka <hrosicka@post.cz>
2023-08-04 20:49:39 +0200

    Kvadratická rovnice - naštení vstupů

---
+++ vypocetKvadRovnice.py
@@ -0,0 +1,27 @@
+print("KVADRATICKÁ ROVNICE")
+print("-------------------")
+
+while True:
+        cislo1 = input("Zadejte koeficient a: ")
+        try:
+            a = float(cislo1)
+            break
+        except ValueError:
+            print("Zadaná hodnota není číslo! Zkus to znovu!")     
+
+while True:
+        cislo2 = input("Zadejte koeficient b: ")
+        try:
+            b = float(cislo2)
+            break
+        except ValueError:
+            print("Zadaná hodnota není číslo! Zkus to znovu!")
+
+while True:
+        cislo3 = input("Zadejte koeficient c: ")
+        try:
+            c = float(cislo3)
+            break
+        except ValueError:
+            print("Zadaná hodnota není číslo! Zkus to znovu!")
+
